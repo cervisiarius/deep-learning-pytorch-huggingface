fcb9dda8f5285c36ed6f5c6406d57d939c00fbef
Robert West <t-bobwest@microsoft.com>
2025-02-07 22:28:34 +0000

    update

--- training/train_cotroller.sh
+++ training/train_cotroller.sh
@@ -1,10 +1,11 @@
 model_id=cotroller_DeepSeek-R1-Distill-Qwen-7B_qlora_ep1
+username=cervisiarius
 
 # Train:
-python3.11 scripts/run_cotroller_training.py --config receipes/cotroller.yaml
+python3.11 scripts/run_cotroller_training.py --config receipes/cotroller_${model_id}.yaml
 
 # Merge weights:
 python3.11 scripts/merge_adapter_weights.py --peft_model_id runs/${model_id} --push_to_hub True --repository_id ${model_id}_MERGED
 
 # Inference:
-docker run --name tgi --gpus 1 -d -ti -p 8080:80 --shm-size=2GB -e HF_TOKEN=$(cat ~/.cache/huggingface/token) ghcr.io/huggingface/text-generation-inference:3.0.1 --model-id cervisiarius/${model_id}_MERGED --num-shard 1
+docker run --name tgi --gpus 1 -d -ti -p 8080:80 --shm-size=2GB -e HF_TOKEN=$(cat ~/.cache/huggingface/token) ghcr.io/huggingface/text-generation-inference:3.0.1 --model-id ${username}/${model_id}_MERGED --num-shard 1
